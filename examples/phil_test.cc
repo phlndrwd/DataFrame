5e1e0a067d3fd8edc22fee573922ed28d83b787d
phlndrwd <philjunderwood@gmail.com>
2024-02-21 15:55:22 +0000

    Removal of old comments.

--- examples/phil_test.cc
+++ examples/phil_test.cc
@@ -101,11 +101,3 @@ int main(int, char *[])  {
 
   return (0);
 }
-
-// -----------------------------------------------------------------------------
-
-// Local Variables:
-// mode:C++
-// tab-width:4
-// c-basic-offset:4
-// End:
